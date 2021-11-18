c12e4264c2f1eb41e386decca9352ab91a974341
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-11-18 19:12:42 -0300

    reparo por causa do lint

--- src/services/Search.js
+++ src/services/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories, getProductsFromCategoryAndQuery } from './api';
+import { getCategories } from './api';
 
 class Search extends React.Component {
   constructor() {
