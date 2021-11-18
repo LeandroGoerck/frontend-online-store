085056fa30d269086338f3590c54a48e0bbc8ec8
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-11-18 15:39:06 -0300

    iniciando requisito 1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          INICIANDO REQUISITO 1
         </a>
       </header>
     </div>
