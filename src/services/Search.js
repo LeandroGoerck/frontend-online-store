69bbce69c4e6f8b10956f70bb6aba018ed601967
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-11-19 22:56:07 -0300

    css style to Search Page

--- src/services/Search.js
+++ src/services/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getCategories, getProductsFromCategoryAndQuery } from './api';
+import './Search.css';
 
 class Search extends React.Component {
   constructor() {
@@ -48,61 +49,82 @@ class Search extends React.Component {
     const { categories, searchInput, results } = this.state;
     const { handleChange, handleSearchButton } = this;
     return (
-      <div>
-        <input
-          data-testid="query-input"
-          type="text"
-          name="searchInput"
-          value={ searchInput }
-          onChange={ handleChange }
-        />
-        <button
-          data-testid="query-button"
-          type="button"
-          onClick={ handleSearchButton }
-        >
-          Pesquisar
-        </button>
-
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-        <Link to="/shoppingcart" data-testid="shopping-cart-button">Carrinho</Link>
-        <div>
-          {categories.length !== 0 && categories
-            .map((category) => (
-              <div key={ category.id }>
-                <label htmlFor={ category.id }>
-                  <input
-                    id={ category.id }
-                    type="radio"
-                    name="category"
-                    value={ category.id }
-                    data-testid="category"
-                    onClick={ handleChange }
-                  />
-                  { category.name }
-                </label>
-              </div>
-            ))}
-        </div>
-
-        <div>
-          { results.length > 0 && results.map((result) => (
-            <div
-              key={ result.id }
-              data-testid="product"
+      <div className="search-page">
+        <section className="search-conteiner">
+          <div>
+            <input
+              className="search-input"
+              data-testid="query-input"
+              type="text"
+              name="searchInput"
+              value={ searchInput }
+              onChange={ handleChange }
+            />
+            <button
+              className="search-button"
+              data-testid="query-button"
+              type="button"
+              onClick={ handleSearchButton }
             >
-              <Link
-                data-testid="product-detail-link"
-                to={ `/product/${result.id}` }
-              >
-                {result.title}
-              </Link>
-              <img src={ result.thumbnail } alt={ result.title } />
-              <p>{`R$${result.price.toFixed(2)}`}</p>
+              Pesquisar
+            </button>
+
+            <Link
+              className="cart-button"
+              to="/shoppingcart"
+              data-testid="shopping-cart-button"
+            >
+              Carrinho
+            </Link>
+
+          </div>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </section>
+
+        <div className="content-conteiner">
+          <section className="category-conteiner">
+            <div>
+              {categories.length !== 0 && categories
+                .map((category) => (
+                  <div key={ category.id }>
+                    <label htmlFor={ category.id }>
+                      <input
+                        id={ category.id }
+                        type="radio"
+                        name="category"
+                        value={ category.id }
+                        data-testid="category"
+                        onClick={ handleChange }
+                      />
+                      { category.name }
+                    </label>
+                  </div>
+                ))}
             </div>
-          )) }
+          </section>
+
+          <section className="search-result-conteiner">
+            <div>
+              { results.length > 0 && results.map((result) => (
+                <div
+                  className="item-card"
+                  key={ result.id }
+                  data-testid="product"
+                >
+                  <Link
+                    data-testid="product-detail-link"
+                    to={ `/product/${result.id}` }
+                  >
+                    {result.title}
+                  </Link>
+                  <img src={ result.thumbnail } alt={ result.title } />
+                  <p>{`R$${result.price.toFixed(2)}`}</p>
+                </div>
+              )) }
+            </div>
+          </section>
         </div>
 
       </div>
